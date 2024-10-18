9b95d95aa585a0ad9215599aba9a1d1791b5c352
Anxiietyx <ivanloong05@gmail.com>
2024-10-19 00:05:48 +0800

    Initial commit

---
+++ client/src/lib/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrR3mB4XBxF3fkOn5qKjxBHwFqPiDgnfo",
+  authDomain: "coffee-8a132.firebaseapp.com",
+  projectId: "coffee-8a132",
+  storageBucket: "coffee-8a132.appspot.com",
+  messagingSenderId: "50411852600",
+  appId: "567cab65cc6642112027cd",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
